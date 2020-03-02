58f9cdd65da75e93fb1c75e79b2b1fb8e1019bd3
Nikolay Matorin <bobkexit@gmail.com>
2020-03-02 11:43:25 +0300

    setup UsersController

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -33,5 +33,6 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     var migrations = MigrationConfig()
     // 4
     migrations.add(model: Acronym.self, database: .psql)
+    migrations.add(model: User.self, database: .psql)
     services.register(migrations)
 }
