52c6f36f663e0b8d6f1e8ee13218dac1d8551065
Nikolay Matorin <bobkexit@gmail.com>
2020-03-03 10:50:59 +0300

    build parent-child relationships between user and acronym

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -14,6 +14,7 @@ struct AcronymsController: RouteCollection {
         acronymsRoutes.get("search", use: searchHandler)
         acronymsRoutes.get("first", use: getFirstHandler)
         acronymsRoutes.get("sorted", use: sortedHandler)
+        acronymsRoutes.get(Acronym.parameter, "user", use: getUserHandler)
     }
     
     func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
@@ -21,7 +22,7 @@ struct AcronymsController: RouteCollection {
     }
     
     func createHandler(_ req: Request, acronym: Acronym) throws -> Future<Acronym> {
-         return acronym.save(on: req)
+        return acronym.save(on: req)
     }
     
     func getHandler(_ req: Request) throws -> Future<Acronym> {
@@ -35,6 +36,7 @@ struct AcronymsController: RouteCollection {
             req.content.decode(Acronym.self)) { acronym, updatedAcronym in
                 acronym.short = updatedAcronym.short
                 acronym.long = updatedAcronym.long
+                acronym.userID = updatedAcronym.userID
                 return acronym.save(on: req)
         }
     }
@@ -63,5 +65,13 @@ struct AcronymsController: RouteCollection {
     
     func sortedHandler(_ req: Request) throws -> Future<[Acronym]> {
         return Acronym.query(on: req).sort(\.short, .ascending).all()
+    }
+    
+    func getUserHandler(_ req: Request) throws -> Future<User> {
+        return try req
+            .parameters.next(Acronym.self)
+            .flatMap(to: User.self) { acronym in
+                acronym.user.get(on: req)
+        }
     }
 }
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -32,7 +32,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(databases)
     var migrations = MigrationConfig()
     // 4
-    migrations.add(model: Acronym.self, database: .psql)
     migrations.add(model: User.self, database: .psql)
+    migrations.add(model: Acronym.self, database: .psql)
     services.register(migrations)
 }
