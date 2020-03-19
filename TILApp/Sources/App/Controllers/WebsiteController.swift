63755b875a3c2537370aae417e2449c2c04a629f
Nikolay Matorin <bobkexit@gmail.com>
2020-03-19 10:18:12 +0300

    added acronym details page

--- TILApp/Sources/App/Controllers/WebsiteController.swift
+++ TILApp/Sources/App/Controllers/WebsiteController.swift
@@ -4,6 +4,7 @@ import Leaf
 struct WebsiteController: RouteCollection {
     func boot(router: Router) throws {
         router.get(use: indexHandler)
+        router.get("acronyms", Acronym.parameter, use: acronymHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
@@ -15,9 +16,28 @@ struct WebsiteController: RouteCollection {
                 return try req.view().render("index", context)
         }
     }
+    
+    func acronymHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(Acronym.self)
+            .flatMap(to: View.self) { acronym in
+                return acronym.user.get(on: req)
+                    .flatMap(to: View.self) { user in
+                        let context = AcronymContext(title: acronym.short,
+                                                     acronym: acronym,
+                                                     user: user)
+                        return try req.view().render("acronym", context)
+                }
+        }
+    }
 }
 
 struct IndexContext: Encodable {
     let title: String
     let acronyms: [Acronym]?
+}
+
+struct AcronymContext: Encodable {
+    let title: String
+    let acronym: Acronym
+    let user: User
 }
