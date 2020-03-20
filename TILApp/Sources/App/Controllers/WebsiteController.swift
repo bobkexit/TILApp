e3408ed49f5ff3eff2b3e4d9f78dff7e021c11db
Bob <bobkexit@gmail.com>
2020-03-20 23:36:02 +0400

    finished chapter-15 (#9)

--- TILApp/Sources/App/Controllers/WebsiteController.swift
+++ TILApp/Sources/App/Controllers/WebsiteController.swift
@@ -5,14 +5,15 @@ struct WebsiteController: RouteCollection {
     func boot(router: Router) throws {
         router.get(use: indexHandler)
         router.get("acronyms", Acronym.parameter, use: acronymHandler)
+        router.get("users", User.parameter, use: userHandler)
+        router.get("users", use: allUsersHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
         return Acronym.query(on: req)
             .all()
             .flatMap(to: View.self) { acronyms in
-                let acronymsData = acronyms.isEmpty ? nil : acronyms
-                let context = IndexContext(title: "Home page", acronyms: acronymsData)
+                let context = IndexContext(title: "Home page", acronyms: acronyms)
                 return try req.view().render("index", context)
         }
     }
@@ -29,15 +30,49 @@ struct WebsiteController: RouteCollection {
                 }
         }
     }
+    
+    func userHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(User.self)
+            .flatMap(to: View.self) { user in
+                return try user.acronyms
+                    .query(on: req)
+                    .all()
+                    .flatMap(to: View.self) { acronyms in
+                        let context = UserContext(title: user.name, user: user, acronyms: acronyms)
+                        return try req.view().render("user", context)
+                    }
+            }
+    }
+    
+    func allUsersHandler(_ req: Request) throws -> Future<View> {
+        return User.query(on: req)
+            .all()
+            .flatMap(to: View.self) { users in
+                let context = AllUsersContext(title: "All Users", users: users)
+                return try req.view().render("allUsers", context)
+        }
+    }
 }
 
 struct IndexContext: Encodable {
     let title: String
-    let acronyms: [Acronym]?
+    let acronyms: [Acronym]
 }
 
 struct AcronymContext: Encodable {
     let title: String
     let acronym: Acronym
     let user: User
+}
+
+struct UserContext: Encodable {
+    let title: String
+    let user: User
+    let acronyms: [Acronym]
+    
+}
+
+struct AllUsersContext: Encodable {
+    let title: String
+    let users: [User]
 }
